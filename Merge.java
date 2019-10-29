b9753dce1b37e2670cbb31dffa3798322112304d
Lalla Sankara <47159210+Lalla22@users.noreply.github.com>
2019-10-29 02:43:04 -0400

    Update Merge.java

--- Merge.java
+++ Merge.java
@@ -1,48 +1,47 @@
-/******************************************************************************
- *  _Example commands for running this file_
- *  Compilation:  javac -Xlint Merge.java
- *  Execution:    java Merge
- *
- *  Example code reverses the input array. Edit this file
- *  to check whether the input is a palindrome.
- *
- ******************************************************************************/
 
-
+/**
+ * Merge two sorted lists into one sorted list.
+ *
+ * @author Lalla Sankara 
+ * @version 10/29/2019
+ */
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+public class Merge
+{
+    // Sample input
+    public static int[] input1 = {1,3,5};
+    public static int[] input2 = {2,4,5,6};
 
-public class Merge {
-
-  // Sample input
-  public static int[] input1 = {1,3,5};
-  public static int[] input2 = {2,4,5,6};
-
-  public static void main(String[] args){
-
-    // Display Input1
-    System.out.println("Input1\t" + Arrays.toString(input1));
-
-    // Create empty queues
-    Queue<Integer> queue1 = new LinkedList<Integer>();
-    Queue<Integer> queue2 = new LinkedList<Integer>();
-
-
-    for(int i=0; i<input1.length; i++){
+    public static void main (String [] args)
+    {
+      // Display Input1
+      System.out.println("Input1\t" + Arrays.toString(input1));
+        
+     // Create empty queues
+     Queue<Integer> queue1 = new LinkedList<Integer>();
+     Queue<Integer> queue2 = new LinkedList<Integer>();
+  
+      for(int i=0; i<input1.length; i++){
       // One by one push all characters of string to queue.
       queue1.add(input1[i]);
+      queue2.add(input2[i]);
+      queue1.add(input2[i]);
+     }
+     
+     // Save the input string
+     String output = "";
+     while(queue1.peek() != null ){
+       //One by one pop all characters from stack and put them back to string.
+       output += Integer.toString(queue1.poll());
+       
+     }
+     
+     // Display reversed as string array
+     System.out.println("Output1\t" + Arrays.toString(output.split("")));
+
+     
     }
-
-    // Save the input string
-    String output = "";
-    while(queue1.peek() != null ){
-      //One by one pop all characters from stack and put them back to string.
-      output += Integer.toString(queue1.poll());
-    }
-
-    // Display reversed as string array
-    System.out.println("Output1\t" + Arrays.toString(output.split("")));
-
-  }
+    
 }
